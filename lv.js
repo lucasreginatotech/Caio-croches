9b9b1413cccc01694847e71dde0474126f6cdae3
lucas <lucasreginato020@gmail.com>
2026-08-31 16:46:27 -0300

    Atualizando site e adicionando nova marca

---
+++ lv.js
@@ -0,0 +1,126 @@
+// LISTA DE PRODUTOS - LOUIS VUITTON
+const produtosLV = [
+    {
+        nome: "Boné Louis Vuitton Azul",
+        categoria: "lv",
+        preco: "R$ 300,00",
+        imagens: [
+            "imagens/louis vitton/lvazul1.jpg",
+            "imagens/louis vitton/lvazul2.jpg"
+        ],
+        whatsapp: "Salve Caio! Quero encomendar o Boné Louis Vuitton Azul."
+    },
+    {
+        nome: "Boné Louis Vuitton Azul Colorido",
+        categoria: "lv",
+        preco: "R$ 300,00",
+        imagens: [
+            "imagens/louis vitton/lvazulcolorido1.jpg",
+            "imagens/louis vitton/lvazulcolorido2.jpg"
+        ],
+        whatsapp: "Salve Caio! Quero encomendar o Boné Louis Vuitton Azul Colorido."
+    },
+    {
+        nome: "Boné Louis Vuitton Azul CP",
+        categoria: "lv",
+        preco: "R$ 300,00",
+        imagens: [
+            "imagens/louis vitton/lvazulcp1.jpg",
+            "imagens/louis vitton/lvazulcp2.jpg",
+            "imagens/louis vitton/lvazulcp3.jpg"
+        ],
+        whatsapp: "Salve Caio! Quero encomendar o Boné Louis Vuitton Azul CP."
+    },
+    {
+        nome: "Boné Louis Vuitton Verde e Preto",
+        categoria: "lv",
+        preco: "R$ 300,00",
+        imagens: [
+            "imagens/louis vitton/lvbr1.jpg",
+            "imagens/louis vitton/lvbr2.jpg"
+        ],
+        whatsapp: "Salve Caio! Quero encomendar o Boné Louis Vuitton Verde e Preto."
+    },
+    {
+        nome: "Boné Louis Vuitton Branco",
+        categoria: "lv",
+        preco: "R$ 300,00",
+        imagens: [
+            "imagens/louis vitton/lvbranco1.jpg"
+        ],
+        whatsapp: "Salve Caio! Quero encomendar o Boné Louis Vuitton Branco."
+    },
+    {
+        nome: "Boné Louis Vuitton Colorido",
+        categoria: "lv",
+        preco: "R$ 300,00",
+        imagens: [
+            "imagens/louis vitton/lvcolorido1.jpg",
+            "imagens/louis vitton/lvcolorido2.jpg",
+            "imagens/louis vitton/lvcolorido3.jpg"
+        ],
+        whatsapp: "Salve Caio! Quero encomendar o Boné Louis Vuitton Colorido."
+    },
+    {
+        nome: "Boné Louis Vuitton Marrom",
+        categoria: "lv",
+        preco: "R$ 300,00",
+        imagens: [
+            "imagens/louis vitton/lvmarrom1.jpg",
+            "imagens/louis vitton/lvmarrom2.jpg"
+        ],
+        whatsapp: "Salve Caio! Quero encomendar o Boné Louis Vuitton Marrom."
+    },
+    {
+        nome: "Boné Louis Vuitton Marrom Claro",
+        categoria: "louis vitton",
+        preco: "R$ 300,00",
+        imagens: [
+            "imagens/louis vitton/lvmarromc1.jpg",
+            "imagens/louis vitton/lvmarromc2.jpg"
+        ],
+        whatsapp: "Salve Caio! Quero encomendar o Boné Louis Vuitton Marrom Claro."
+    },
+    {
+        nome: "Boné Louis Vuitton Preto",
+        categoria: "lv",
+        preco: "R$ 300,00",
+        imagens: [
+            "imagens/louis vitton/lvpreto1.jpg",
+            "imagens/louis vitton/lvpreto2.jpg",
+            "imagens/louis vitton/lvpreto3.jpg"
+        ],
+        whatsapp: "Salve Caio! Quero encomendar o Boné Louis Vuitton Preto."
+    },
+    {
+        nome: "Boné Louis Vuitton Vermelho",
+        categoria: "lv",
+        preco: "R$ 300,00",
+        imagens: [
+            "imagens/louis vitton/lvvermelho1.jpg",
+            "imagens/louis vitton/lvvermelho2.jpg",
+            "imagens/louis vitton/lvvermelho3.jpg"
+        ],
+        whatsapp: "Salve Caio! Quero encomendar o Boné Louis Vuitton Vermelho."
+    },
+    {
+        nome: "Boné Louis Vuitton All Black",
+        categoria: "lv",
+        preco: "R$ 300,00",
+        imagens: [
+            "imagens/louis vitton/lvallblack1.jpg",
+            "imagens/louis vitton/lvallblack2.jpg"
+        ],
+        whatsapp: "Salve Caio! Quero encomendar o Boné Louis Vuitton All Black."
+    },
+    {
+        nome: "Boné Louis Vuitton PV",
+        categoria: "lv",
+        preco: "R$ 300,00",
+        imagens: [
+            "imagens/louis vitton/lvpv1.jpg",
+            "imagens/louis vitton/lvpv2.jpg"
+        ],
+        whatsapp: "Salve Caio! Quero encomendar o Boné Louis Vuitton PV."
+    }
+];
